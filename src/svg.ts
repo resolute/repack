e1732cfe604e484de02305bc5090336868a437d0
Adam Chalemian <adam.chalemian@resolute.com>
2020-03-03 11:56:02 -0500

    handle SVGs passed by URL

--- src/svg.ts
+++ src/svg.ts
@@ -4,10 +4,12 @@ import probe = require('probe-image-size');
 const Svgo = require('svgo');
 
 const svg = (asset) => async (input) => {
-  const { filename } = input;
   try {
-    // const content = fs.readFileSync(filename, 'utf-8');
-    const content = await fs.promises.readFile(filename, 'utf-8');
+    let incomingData = input.data;
+    if (!incomingData) {
+      const { filename } = input;
+      incomingData = await fs.promises.readFile(filename, 'utf-8');
+    }
     const { data } = await new Svgo({
       // datauri: 'unenc', // our regex escaping is better than encodeURIComponent()
       plugins: [
@@ -18,13 +20,13 @@ const svg = (asset) => async (input) => {
         { removeDimensions: true },
       ],
     })
-      .optimize(content, {}); // info param required by svgo@1.3.1
+      .optimize(incomingData, {}); // info param required by svgo@1.3.1
     const { width, height } = probe.sync(Buffer.from(data));
     return {
       ...input, data, width, height,
     };
   } catch (error) {
-    process.emitWarning(`${filename}: ${error}`);
+    process.emitWarning(`${input.filename}: ${error}`);
     return input;
   }
 };
